28318ffe23735aac2d2aa1368cf62b3a46019393
TanvirulAbrar <abrartanvirul@gmail.com>
2026-02-26 16:08:20 +0600

    update router.jsx

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,49 +1,23 @@
-import { createBrowserRouter, Navigate } from "react-router";
-import MainLayout from "../layouts/MainLayout";
-import Login from "../pages/Auth/Login/Login";
-import Register from "../pages/Auth/Register/Register";
-import ErrorPage from "../pages/Shared/404/ErrorPage";
-import Loading from "../pages/Shared/Loading/Loading";
-import Home from "../pages/Home/Home/Home";
-import DashboardHome from "../pages/Dashboard/DashboardHome/DashboardHome";
-import DashboardOverview from "../pages/Dashboard/Overview/DashboardOverview";
-
-import AlTicket from "../pages/allTicket/AlTicket";
-import TicketDetail from "../pages/allTicket/Detail/TicketDetail";
-import PrivateRoute from "./PrivateRoute";
-import PaymentHistory from "../pages/Dashboard/PaymentHistory/PaymentHistory";
-import Profile from "../pages/Dashboard/profile/Profile";
-import MyBookedTicket from "../pages/Dashboard/MyTicket/MyBookedTicket";
-import AddTicket from "../pages/Dashboard/Add ticket/AddTicket";
-import MyAddedTicket from "../pages/Dashboard/MyAddedTicket/MyAddedTicket";
-import UpdateTicket from "../pages/Dashboard/UpdateTicket/UpdateTicket";
-import RequestBooking from "../pages/Dashboard/RequestedBooking/RequestBooking";
-import PaymentSuccess from "../pages/Dashboard/Payment/PaymentSuccess";
-import PaymentCancelled from "../pages/Dashboard/Payment/PaymentCancelled";
-import Payment from "../pages/Dashboard/Payment/Payment";
-import RevenueOverview from "../pages/Dashboard/RevenueOverview/RevenueOverview";
-import ManageTicket from "../pages/Dashboard/Manage ticket/ManageTicket";
-import ManageUser from "../pages/Dashboard/Manage User/ManageUser";
-import Advertise from "../pages/Dashboard/Advertise/Advertise";
-import AdminRoute from "./AdminRoute";
-import VendorRoute from "./VendorRoute";
-
-// Additional Content Pages
-import About from "../pages/About/About";
-import Contact from "../pages/Contact/Contact";
-import Blog from "../pages/Blog/Blog";
-import Help from "../pages/Help/Help";
-import Privacy from "../pages/Privacy/Privacy";
-import Terms from "../pages/Terms/Terms";
-import Showcase1 from "../pages/Showcase/Showcase1";
-import Showcase2 from "../pages/Showcase/Showcase2";
-import Showcase3 from "../pages/Showcase/Showcase3";
-import Showcase4 from "../pages/Showcase/Showcase4";
-import TicketDetails from "../pages/allTicket/Detail/TicketDetails";
+import { createBrowserRouter } from "react-router";
+import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
+import DashboardPage from "../pages/DashboardPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    Component: MainLayout,
+    Component: DashboardPage,
+  },
+  {
+    path: "/login",
+    Component: LoginPage,
+  },
+  {
+    path: "/signup", 
+    Component: SignupPage,
+  },
+  {
+    path: "/dashboard",
+    Component: DashboardPage,
   },
 ]);
